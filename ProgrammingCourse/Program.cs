8c5a1087440cb89f894ae6f77960f5e4dafd3c3d
varsanyib <109478771+varsanyib@users.noreply.github.com>
2022-11-21 09:27:41 +0100

    Add project files.

---
+++ ProgrammingCourse/Program.cs
@@ -0,0 +1,78 @@
+﻿using ProgrammingCourse.SajatOsztaly;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCourse
+{
+    class Program
+    {
+        static List<Hallgato> hallgatok = new List<Hallgato>();
+        
+        static void Main(string[] args)
+        {
+            MasodikFeladat();
+            HarmadikFeladat();
+            NegyedikFeladat();
+            OtodikFeladat();
+            HatodikFeladat();
+
+            Console.ReadKey();
+        }
+
+        private static void HatodikFeladat()
+        {
+            Console.WriteLine("6. feladat:\n\tÁllásajánlatot kap:");
+            foreach (Hallgato e in hallgatok.Where(x => x.SikeresVizsga).Where(y => y.Finanszirozas == 2).Where(y => y.EddigBefizetett == 0).OrderByDescending(y => y.OsszPont))
+            {
+                Console.WriteLine($"\t{e.Nev}\t\t{e.OsszPont}");
+            }
+        }
+
+        private static void OtodikFeladat()
+        {
+            Console.WriteLine("5. feladat:\n\tA következő tanulóknak van elmaradása: ");
+            foreach (Hallgato e in hallgatok)
+            {
+                if (e.Finanszirozas == 0 && e.EddigBefizetett < 2600)
+                {
+                    Console.WriteLine($"\t{e.Nev}");
+                }
+                if (e.Finanszirozas == 1 && e.EddigBefizetett < (Hallgato.targyHo * 312))
+                {
+                    Console.WriteLine($"\t{e.Nev}");
+                }
+                if (e.Finanszirozas == 2 && Hallgato.targyHo >= 10 && e.EddigBefizetett < 4000)
+                {
+                    Console.WriteLine($"\t{e.Nev}");
+                }
+            }
+        }
+
+        private static void NegyedikFeladat()
+        {
+            double ferfiAtlag = (hallgatok.Where(x => x.Ferfi).Average(y => y.EredmenyProg) + hallgatok.Where(x => x.Ferfi).Average(y => y.EredmenyMest) + hallgatok.Where(x => x.Ferfi).Average(y => y.EredmenyGraf) + hallgatok.Where(x => x.Ferfi).Average(y => y.EredmenyArch))/ 4;
+            double noAtlag = (hallgatok.Where(x => !x.Ferfi).Average(y => y.EredmenyProg) + hallgatok.Where(x => !x.Ferfi).Average(y => y.EredmenyMest) + hallgatok.Where(x => !x.Ferfi).Average(y => y.EredmenyGraf) + hallgatok.Where(x => !x.Ferfi).Average(y => y.EredmenyArch)) / 4;
+            Console.WriteLine($"4. feladat:\n\tFiúk   átlagteljesítménye: {Math.Round(ferfiAtlag, 2)}%\n\tLányok átlagteljesítménye: {Math.Round(noAtlag, 2)}%");
+        }
+
+        private static void HarmadikFeladat()
+        {
+            Console.WriteLine($"3. feladat:\n\tA tanfolyamra {hallgatok.Count} fő iratkozott be.");
+        }
+
+        private static void MasodikFeladat()
+        {
+            using (StreamReader sr = new StreamReader("coursedata.csv"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    hallgatok.Add(new Hallgato(sr.ReadLine()));
+                }
+            }
+        }
+    }
+}
---
+++ ProgrammingCourse/SajatOsztaly/Hallgato.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProgrammingCourse.SajatOsztaly
+{
+    class Hallgato
+    {
+        public string Nev { get; private set; }
+        public bool Ferfi { get; private set; }
+        public int Finanszirozas { get; private set; }
+        public int EddigBefizetett { get; private set; }
+        public int EredmenyProg { get; private set; }
+        public int EredmenyGraf { get; private set; }
+        public int EredmenyArch { get; private set; }
+        public int EredmenyMest { get; private set; }
+        public double Atlag { get; private set; }
+        public bool SikeresVizsga { get; private set; }
+        public int OsszPont { get; private set; }
+
+        public const int targyHo = 5;
+        public Hallgato(string sor)
+        {
+           
+
+            string[] tmp = sor.Split(';');
+            Nev = tmp[0];
+            Ferfi = (tmp[1] == "m");
+            Finanszirozas = Convert.ToInt32(tmp[2]);
+            EddigBefizetett = Convert.ToInt32(tmp[3].Split('$')[1]);
+            EredmenyProg = Convert.ToInt32(tmp[4]);
+            EredmenyGraf = Convert.ToInt32(tmp[5]);
+            EredmenyArch = Convert.ToInt32(tmp[6]);
+            EredmenyMest = Convert.ToInt32(tmp[7]);
+
+            if (EredmenyArch >= 51 && EredmenyMest >= 51 && EredmenyGraf >= 51 && EredmenyProg >= 51)
+            {
+                SikeresVizsga = true;
+            }
+            else
+            {
+                SikeresVizsga = false;
+            }
+
+            OsszPont = EredmenyArch + EredmenyMest + EredmenyGraf + EredmenyProg;
+
+            Atlag = (EredmenyProg + EredmenyGraf + EredmenyArch + EredmenyMest) / 4;
+
+        }
+
+    }
+}
